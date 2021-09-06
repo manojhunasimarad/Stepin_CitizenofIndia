1402edaafc760f97d1699f507871d2e15927055f
manojhunasimarad <89728596+manojhunasimarad@users.noreply.github.com>
2021-09-06 11:58:17 +0530

    Update test_VMS.c

--- 3_Implementation/test/test_VMS.c
+++ 3_Implementation/test/test_VMS.c
@@ -1,1 +1,48 @@
+#include "VMS.h"
+#include "unity.h"
 
+
+/* Required by the unity test framework */
+void setUp()
+{
+}
+/* Required by the unity test framework */
+void tearDown()
+{
+}
+
+int test_read()
+{
+    // Can ommi the below intialization as it is done at the declaration time
+    
+     
+}
+int test_add()
+{
+    
+}
+
+int test_view()
+{
+   
+
+}
+int  test_search()
+{
+
+}
+
+int main(void)
+{
+    /* Initiate the Unity Test Framework */
+    UNITY_BEGIN();
+
+    /* Run Test functions */
+    RUN_TEST(test_read);
+    RUN_TEST(test_add);
+    RUN_TEST(test_view);
+    RUN_TEST(test_search);
+
+    /* Close the Unity Test Framework */
+    return UNITY_END();
+}
